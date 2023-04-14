a483c9b5ab7e34a4c0a850a01af29211fe7aae44
Acco <1178004264@qq.com>
2023-04-14 18:23:57 +0800

    init

---
+++ src/main/java/ysu/szx/ysuacmoj/YsuacmojApplication.java
@@ -0,0 +1,13 @@
+package ysu.szx.ysuacmoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsuacmojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YsuacmojApplication.class, args);
+	}
+
+}
