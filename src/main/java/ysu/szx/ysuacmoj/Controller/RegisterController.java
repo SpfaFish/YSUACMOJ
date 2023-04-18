1cdfb58c3b15603b481b8ac52d935d3440cbb162
Acco <1178004264@qq.com>
2023-04-18 20:33:51 +0800

    chan json

--- src/main/java/ysu/szx/ysuacmoj/Controller/RegisterController.java
+++ src/main/java/ysu/szx/ysuacmoj/Controller/RegisterController.java
@@ -3,10 +3,13 @@ package ysu.szx.ysuacmoj.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ysu.szx.ysuacmoj.Peo.Stu;
 import ysu.szx.ysuacmoj.Pojo.Results;
 import ysu.szx.ysuacmoj.Service.RegisterService;
+
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -16,7 +19,11 @@ public class RegisterController {
     RegisterService registerService;
 
     @PostMapping("/regist")
-    public Results Regist(String id, String name, String nickName, String password){
+    public Results Regist(@RequestBody Map<String, Object> request){
+        String id = (String)request.get("id");
+        String name = (String)request.get("name");
+        String nickName = (String)request.get("nickName");
+        String password = (String)request.get("password");
         Stu stu = new Stu(id, name, nickName, password, (float)1200);
         return registerService.Insert(stu);
     }
